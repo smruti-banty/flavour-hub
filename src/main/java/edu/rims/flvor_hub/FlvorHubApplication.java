d10daa3805172119a55fbfcc71db896c99ed8b3a
smruti <smruti@gmail.com>
2025-01-18 11:42:38 +0530

    Initial env setup

---
+++ src/main/java/edu/rims/flvor_hub/FlvorHubApplication.java
@@ -0,0 +1,13 @@
+package edu.rims.flvor_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlvorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlvorHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rims/flvor_hub/FlvorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rims.flvor_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlvorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
